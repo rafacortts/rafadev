196955f7f94f770fabd6f297e80be7d6d02d04d6
rafacortts <rafaelcorttes1@gmail.com>
2024-05-17 09:52:36 -0300

    Alteração-BG

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 const Hero = () => {
   return (
-    <div className="bg-blue-00 shadow-md">
+    <div className="bg-blue-00 shadow-md bg-gradient-animated">
       <main className=" sm:mt-0 container ">
         <div className=" min-h-[500px] flex items-center justify-center">
           <div className="space-y-5 justify-center p-12 ">
@@ -19,7 +19,7 @@ const Hero = () => {
             <div className="space-x-4 flex justify-center">
               <a
                 href="#"
-                className="bg-blue-700 border-2 border-blue-700 text-white p-2 rounded-full transition hover:bg-white hover:text-black hover:border-2 hover:border-blue-700"
+                className="bg-blue-700 border-2 border-blue-700 text-white p-2 rounded-full transition hover:bg-transparent hover:text-black hover:border-2 hover:border-blue-700"
               >
                 {" "}
                 Entre em Contato
